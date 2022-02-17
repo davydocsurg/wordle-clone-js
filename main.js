f6e7ae1822a9c2219376743df1679b8bfcc2e0ec
David Chibueze Ndubuisi <davydocsurg@gmail.com>
2022-02-17 17:03:29 +0100

    add keys

--- main.js
+++ main.js
@@ -11,3 +11,45 @@ document.querySelector("#app").innerHTML = `
 <div class="key-container"></div>
 </div>
 `;
+
+const tileDisplay = document.querySelector(".tile-container");
+const keyboard = document.querySelector(".key-container");
+
+const keys = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "ENTER",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "«",
+];
+
+keys.forEach((key) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = key;
+  buttonElement.setAttribute("id", key);
+  buttonElement.addEventListener("click", () => handleClick(key));
+  keyboard.append(buttonElement);
+});
