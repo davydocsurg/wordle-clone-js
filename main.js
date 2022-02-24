a344ab62af7a88120ffe43dda9edfa67374fe6bf
David Chibueze Ndubuisi <davydocsurg@gmail.com>
2022-02-24 15:07:18 +0100

    refractor code: the flipTile()

--- main.js
+++ main.js
@@ -134,8 +134,8 @@ const checkRow = () => {
     console.log("guess == " + guess, "wordle ==" + wordle);
     flipTile();
     if (wordle == guess) {
-      displayMsg("Superb!");
       isGameOver = true;
+      displayMsg("Superb!");
       return;
     }
   } else {
@@ -171,21 +171,48 @@ const addColorToKey = (keyLetter, color) => {
 
 const flipTile = () => {
   const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
+  let checkWordle = wordle;
+  const guess = [];
+
+  rowTiles.forEach((tile, i) => {
+    guess.push({
+      letter: tile.getAttribute("data"),
+      color: "grey-overlay",
+    });
+  });
+
+  guess.forEach((guess, i) => {
+    if (guess.letter == wordle[i]) {
+      guess.color = "yellow-overlay";
+      checkWordle = checkWordle.replace(guess.letter, "");
+    }
+  });
+
+  guess.forEach((guess) => {
+    if (checkWordle.includes(guess.letter)) {
+      guess.color = "yellow-overlay";
+      checkWordle = checkWordle.replace(guess.letter, "");
+    }
+  });
+
   rowTiles.forEach((tile, i) => {
     const dataLetter = tile.getAttribute("data");
 
     setTimeout(() => {
       tile.classList.add("flip");
-      if (dataLetter == wordle[i]) {
-        tile.classList.add("green-overlay");
-        addColorToKey(dataLetter, "green-overlay");
-      } else if (wordle.includes(dataLetter)) {
-        tile.classList.add("yellow-overlay");
-        addColorToKey(dataLetter, "yellow-overlay");
-      } else {
-        tile.classList.add("grey-overlay");
-        addColorToKey(dataLetter, "grey-overlay");
-      }
+      tile.classList.add(guess[i].color);
+      addColorToKey(guess[i].letter, guess[i].color);
+
+      // if (dataLetter == wordle[i]) {
+      //   tile.classList.add("green-overlay");
+      //   addColorToKey(dataLetter, "green-overlay");
+      // } else if (wordle.includes(dataLetter)) {
+      //   tile.classList.add("yellow-overlay");
+      //   addColorToKey(dataLetter, "yellow-overlay");
+      // } else {
+      //   tile.classList.add("grey-overlay");
+      //   addColorToKey(dataLetter, "grey-overlay");
+      // }
     }, 500 * i);
   });
 };
